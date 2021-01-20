1fb33f39bb0e9a9cd741afe5937ea2b5964e4816
hakan turğay <turgayh@itu.edu.tr>
2021-01-21 02:18:48 +0300

    redis config

--- src/helper/redis-db.js
+++ src/helper/redis-db.js
@@ -1,4 +1,4 @@
 const redis = require("redis");
-const redisClient = redis.createClient('rediss://default:v82c7mddiahw6m9v@db-redis-nyc1-80147-do-user-8597379-0.b.db.ondigitalocean.com:25061');
-
+require('dotenv').config();
+const redisClient = redis.createClient(process.env.redisConnectionString);
 module.exports = { redisClient }
