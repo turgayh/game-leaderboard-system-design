5f22f7006e1b19bad27f2ed1fa4f9847d61302a3
hakan turğay <turgayh@itu.edu.tr>
2021-01-25 17:01:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -25,37 +25,3 @@ app.use(errorHandler);
 app.listen(process.argv[2] || process.env.PORT || 4500, () => {
   console.log(`Uygulama ayakta ve ${process.argv[2] || process.env.PORT || 4500} nolu porttan dinlemede.`);
 });
-
-// if (cluster.isMaster) {
-//     console.log('Master PID: ' + process.pid);
-//     for (var i = 0; i < cpuCount; i++) {
-//         cluster.fork();
-//     }
-
-//     cluster.on('fork', function (worker) {
-//         console.log('\tfork (worker ' + worker.process.pid + ')');
-//     });
-
-//     cluster.on('online', function (worker) {
-//         console.log('\tonline (worker ' + worker.process.pid + ')');
-//     })
-
-//     cluster.on('listening', function (worker, address) {
-//         console.log('\tlistening (worker ' + worker.id + ') pid ' + worker.process.pid + ', ' + address.address + ':' + address.port + ')');
-//     });
-
-//     cluster.on('exit', function (worker) {
-//         console.log('\texit (worker ' + worker.process.pid + ')');
-//     });
-
-// } else {
-
-//     console.log('Worker # has been' + process.pid + ' started.');
-//     // start server
-//     app.listen(process.argv[2] || process.env.PORT || 4500, () => {
-//         console.log(`Uygulama ayakta ve ${process.argv[2] || process.env.PORT || 4500} nolu porttan dinlemede.`);
-//     });
-// }
-
-
-
